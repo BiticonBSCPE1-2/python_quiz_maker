aec6c80b99d3c11c2cbe3e24b33213451e0c5d1a
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-04-10 23:31:21 +0800

    Pseudocode

---
+++ python_quiz_maker.py
@@ -0,0 +1,4 @@
+# define a function to save question, options, and answer to a file
+# display a title and set the output filename
+# loop: ask for question, options (a-d), and the correct answer
+# save the question to file and ask if the user wants to continue or stop
