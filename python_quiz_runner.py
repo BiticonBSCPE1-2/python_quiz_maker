dfff9c4e4d42769564271efaad94f87372a02892
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-05-07 23:09:14 +0800

    Adding load functionality to read the txt.file of the quiz maker

--- python_quiz_runner.py
+++ python_quiz_runner.py
@@ -1,5 +1,18 @@
 # import the random library to shuffle questions
+import random
+
 # load the questions from file
+def load_questions(filename):
+    with open(filename, "r") as file:
+        content = file.read()
+    blocks = content.strip().split("---\n")
+    quizzes = []
+    for block in blocks:
+        lines = block.strip().split("\n")
+        if len(lines) >= 6:
+            quizzes.append(lines)
+    return quizzes
+
 # set the filename and load the questions
 # shuffle the questions randomly
 # set the initial score to 0 and a welcome message to the user
