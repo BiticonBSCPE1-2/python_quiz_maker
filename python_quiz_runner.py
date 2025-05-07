34c3c115b558cb033f2167e677bceccc6d65ea48
Angelo Raphael Biticon <angelobiticon@gmail.com>
2025-05-07 23:01:40 +0800

    Pseudocode

---
+++ python_quiz_runner.py
@@ -0,0 +1,9 @@
+# import the random library to shuffle questions
+# load the questions from file
+# set the filename and load the questions
+# shuffle the questions randomly
+# set the initial score to 0 and a welcome message to the user
+# go through each question and ask the user and its answer
+# check if the answer is correct and update the score
+# ask the user if they want to continue
+# show the final score after the quiz ends
